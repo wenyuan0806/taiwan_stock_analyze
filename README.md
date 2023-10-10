8d65f94c69d4de57e67c5806a98d2435dc73e904
wenyuan0806 <wenyuan.0806@gmail.com>
2023-10-11 00:42:01 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# taiwan_stock_analyze
+taiwan_stock_analyze
+========
+
 台灣上市上櫃股票分析，透過爬蟲抓取台灣股市資料
--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import pandas as pd
 set_option('display.unicode.ambiguous_as_wide', True)
 set_option('display.unicode.east_asian_width', True)
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     # -----------------------------------------------------------
     # crawler.getAllStockHistories() 使用教學
     # -----------------------------------------------------------
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     #   6. '受益證券-不動產投資信託'
     #   7. '受益證券-資產基礎證券'
     # -----------------------------------------------------------
-    markets = ['上市', '上櫃']
-    types = ['股票', '臺灣存託憑證(TDR)', 'ETF', 'ETN', '特別股', '受益證券-不動產投資信託', '受益證券-資產基礎證券']
-    crawler.getAllStockHistories(markets, types) # 根據指定的市場別(markets)和股票類型(types)來抓取其所有個股之歷史股價(從 2017-02-06 開始)，並儲存至 /output/歷史股價/
+    # markets = ['上市', '上櫃']
+    # types = ['股票', '臺灣存託憑證(TDR)', 'ETF', 'ETN', '特別股', '受益證券-不動產投資信託', '受益證券-資產基礎證券']
+    # crawler.getAllStockHistories(markets, types) # 根據指定的市場別(markets)和股票類型(types)來抓取其所有個股之歷史股價(從 2017-02-06 開始)，並儲存至 /output/歷史股價/
 
     # crawler.getRealTime(sid=['^TWII', '5351', '3037']) # 抓取指定個股的即時價格
